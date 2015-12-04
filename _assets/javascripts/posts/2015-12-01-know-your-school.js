7b8da3e6ec780fb6a639bfe53bcb9399d9f38c96
Taylor Brushwyler <tbrushwyler@ostusa.com>
2015-12-04 10:51:14 -0500

    Add Benton Harbor 2014 expenditures

--- _assets/javascripts/posts/2015-12-01-know-your-school.js
+++ _assets/javascripts/posts/2015-12-01-know-your-school.js
@@ -1,5 +1,5 @@
-var width = 960,
-    height = 700,
+var width = 300,
+    height = 300,
     radius = Math.min(width, height) / 2;
 
 var x = d3.scale.linear()
@@ -8,81 +8,123 @@ var x = d3.scale.linear()
 var y = d3.scale.sqrt()
     .range([0, radius]);
 
-var catColor = d3.scale.category20c();
-var color = function(d) {
-  if (d.children && d.children.length === 1)
-    return color(d.children[0]);
+var numNodes = function(nodes) {
+  if (nodes.children)
+    return 1 + d3.sum(nodes.children, numNodes);
 
+  return 1;
+};
+
+var rgb = d3.rgb.valueOf();
+var catColor = d3.scale.category10();
+var color = function(d) {
+  // // darken as you move out
+  // if (d.parent && d.parent.parent) {
+  //   var baseColor = color(d.parent);
+  //   var index = d.parent.children.indexOf(d);
+
+  //   return rgb(baseColor).darker((index + 1) * .25);
+  // }
+
+  // return catColor(d.name);
+
+
+  // // darken leaves
+  // if (d.children) {
+  //   return catColor(d.name);
+  // }
+
+  // var baseColor = color(d.parent);
+  // var index = d.parent.children.indexOf(d);
+  
+  // return rgb(baseColor).darker((index + 1) * .33);
+
+  // // node method
+  // // this is a little fucked up because d.parent.children doesn't always contain d
+  // if (!d.parent || !d.parent.parent)
+  //   return catColor(d.name);
+
+  // var multiplier = 0.1;
+  // var baseColor = color(d.parent);
+  // var index = d.parent.children.indexOf(d);
+  // var prevSiblings = d.parent.children.splice(0, index + 1);
+
+  // var numPrevNodes = d3.sum(prevSiblings, numNodes);
+  // return rgb(baseColor).darker(multiplier * numPrevNodes);
+
+  // regular category color
   return catColor(d.name);
 };
 
-var svg = d3.select("#lakeshore-2015").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+var createChart = function(selector, url) {
+  var svg = d3.select(selector).append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var tooltip = d3.select('#lakeshore-2015').append('div')
-  .attr('class', 'tooltip')
-  .style('opacity', 0);
+  var tooltip = d3.select(selector).append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
 
-var partition = d3.layout.partition()
-    .value(function(d) { return d.actual; })
-    .sort(function(a, b) {
-      return d3.ascending(a.depth, b.depth);
-    });
-
-var arc = d3.svg.arc()
-    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
-    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
-
-var format = d3.format('$,');
-
-var value = function(d, propertyName) {
-    if (d.children) {
-        return d3.sum(d.children, function(inner) { return value(inner, propertyName); });
-    }
-
-    return d[propertyName];
-};
-
-d3.json("/data/lakeshore/2015.json", function(error, root) {
-  if (error) throw error;
-
-  var path = svg.selectAll("path")
-      .data(partition.nodes(root))
-    .enter().append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d); })
-      .attr('name', function(d) { return d.name; })
-      .attr('actual', function(d) { return value(d, 'actual'); })
-      .attr('originalBudget', function(d) { return value(d, 'originalBudget'); })
-      .attr('finalBudget', function(d) { return value(d, 'finalBudget'); })
-      .on("click", click)
-      .on('mouseover', function(d) {
-        tooltip.transition()
-          .duration(200)
-          .style('opacity', 0.9);
-        tooltip.html('<strong>' + d.name + ':</strong> ' + format(value(d, 'actual')))
-          .style('left', (d3.event.pageX) + 'px')
-          .style('top', (d3.event.pageY - 28) + 'px');
-      })
-      .on('mouseout', function(d) {
-        tooltip.transition()
-          .duration(500)
-          .style('opacity', 0);
+  var partition = d3.layout.partition()
+      .value(function(d) { return d.actual; })
+      .sort(function(a, b) {
+        return d3.ascending(a.depth, b.depth);
       });
 
-  function click(d) {
-    path.transition()
-      .duration(750)
-      .attrTween("d", arcTween(d));
-  }
-});
+  var arc = d3.svg.arc()
+      .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x))); })
+      .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x + d.dx))); })
+      .innerRadius(function(d) { return Math.max(0, y(d.y)); })
+      .outerRadius(function(d) { return Math.max(0, y(d.y + d.dy)); });
 
-d3.select(self.frameElement).style("height", height + "px");
+  var format = d3.format('$,');
+
+  var value = function(d, propertyName) {
+      if (d.children) {
+          return d3.sum(d.children, function(inner) { return value(inner, propertyName); });
+      }
+
+      return d[propertyName];
+  };
+
+  d3.json(url, function(error, root) {
+    if (error) throw error;
+
+    var path = svg.selectAll("path")
+        .data(partition.nodes(root))
+      .enter().append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d); })
+        .attr('name', function(d) { return d.name; })
+        .attr('actual', function(d) { return value(d, 'actual'); })
+        .attr('originalBudget', function(d) { return value(d, 'originalBudget'); })
+        .attr('finalBudget', function(d) { return value(d, 'finalBudget'); })
+        .on("click", click)
+        .on('mouseover', function(d) {
+          tooltip.transition()
+            .duration(200)
+            .style('opacity', 0.9);
+          tooltip.html('<strong>' + d.name + ':</strong> ' + format(value(d, 'actual')))
+            .style('left', (d3.event.pageX) + 'px')
+            .style('top', (d3.event.pageY - 28) + 'px');
+        })
+        .on('mouseout', function(d) {
+          tooltip.transition()
+            .duration(500)
+            .style('opacity', 0);
+        });
+
+    function click(d) {
+      path.transition()
+        .duration(750)
+        .attrTween("d", arcTween(d));
+    }
+  });
+
+  d3.select(self.frameElement).style("height", height + "px");
+};
 
 // Interpolate the scales!
 function arcTween(d) {
@@ -95,3 +137,6 @@ function arcTween(d) {
         : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
   };
 }
+
+createChart('#lakeshore-2015 .sunburst-chart', '/data/lakeshore/2015.json');
+createChart('#benton-harbor-2014 .sunburst-chart', '/data/benton-harbor/2014.json');
--- _posts/2015/12-december/2015-12-01-know-your-school.md
+++ _posts/2015/12-december/2015-12-01-know-your-school.md
@@ -5,8 +5,21 @@ date: 2015-12-01 12:03:00
 permalink: "/know-your-school"
 ---
 
-<div id="flares"></div>
-<div id="lakeshore-2015"></div>
+<h2>Expenditures</h2>
+<div id="lakeshore-2015" class="chart-container">
+	<h3>
+		Lakeshore Public Schools
+		<small>2015</small>
+	</h3>
+	<div class="sunburst-chart"></div>
+</div>
+<div id="benton-harbor-2014" class="chart-container">
+	<h3>
+		Benton Harbor Area Schools
+		<small>2014</small>
+	</h3>
+	<div class="sunburst-chart"></div>
+</div>
 
 {% js vendor/d3-3.5.10/d3 %}
 {% js posts/2015-12-01-know-your-school %}
